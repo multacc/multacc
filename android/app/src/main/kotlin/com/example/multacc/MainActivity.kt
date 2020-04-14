9bcf591476c4a4ce1f4c3b9823d24d88a3cd8893
micah-white <mcwhite9@crimson.ua.edu>
2020-04-14 17:53:43 -0500

    Button now gives user option to make multacc the default SMS handler; Icon dissappears from the app list though

--- android/app/src/main/kotlin/com/example/multacc/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/multacc/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity: FlutterActivity() {
     super.configureFlutterEngine(flutterEngine)
 	// GeneratedPluginRegistrant.registerWith(new ShimPluginRegistry(flutterEngine));
 	
-	MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+	  MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
       // Note: this method is invoked on the main thread.
       	call, result ->
     //   if (call.method == "getBatteryLevel") {
@@ -36,31 +36,58 @@ class MainActivity: FlutterActivity() {
     //     } else {
     //       result.error("UNAVAILABLE", "Battery level not available.", null)
     //     }
-		if(call.method == "defaultSMS"){
-			// val hello: Int
-      // hello = 5
-			val setSmsAppIntent = Intent(Telephony.Sms.Intents.ACTION_CHANGE_DEFAULT)
-			setSmsAppIntent.putExtra(Telephony.Sms.Intents.EXTRA_PACKAGE_NAME, packageName)
-			startActivityForResult(setSmsAppIntent, 1)
-		}
+        if(call.method == "defaultSMS"){
+          // val hello: Int
+          // hello = 5
+          val setSmsAppIntent = Intent(Telephony.Sms.Intents.ACTION_CHANGE_DEFAULT)
+          setSmsAppIntent.putExtra(Telephony.Sms.Intents.EXTRA_PACKAGE_NAME, packageName)
+          startActivityForResult(setSmsAppIntent, 1)
+        }
        	else {
         	result.notImplemented()
       	}
     }
 
   }
+  
+  // override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+  //   super.onActivityResult(requestCode, resultCode, data)
+  //   if (requestCode != Activity.RESULT_OK){
+  //       return
+  //   }
+  //   when(resultCode){
+  //       REQUEST_DEFAULT_APP -> //user allowed the app to become default
+  //   }
+  // }
 
-  private fun getBatteryLevel(): Int {
-    val batteryLevel: Int
-    if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
-      val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
-      batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
-    } else {
-      val intent = ContextWrapper(applicationContext).registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
-      batteryLevel = intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
-    }
+  // override fun onRequestPermissionsResult(requestCode: Int,
+  //   permissions: Array<String>, grantResults: IntArray) {
+  //     val granted = if(permission.checkPermissionGranted(requestCode, permissions, grantResults)) "permission granted" else "permission not granted"
+  //     Toast.makeText(this,granted, Toast.LENGTH_SHORT).show()
+  //   }
+  
+  // fun checkPermissionGranted(requestCode: Int, permissions: Array<String>, grantResults: IntArray): Boolean{
+  //       when (requestCode) {
+  //           MY_PERMISSIONS_REQUEST_SMS -> {
+  //               // If request is cancelled, the result arrays are empty.
+  //               return (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+  //           }
+  //       }
+  //       return false
+  // }
+  
 
-    return batteryLevel
-  }
+  // private fun getBatteryLevel(): Int {
+  //   val batteryLevel: Int
+  //   if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+  //     val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+  //     batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+  //   } else {
+  //     val intent = ContextWrapper(applicationContext).registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+  //     batteryLevel = intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+  //   }
+
+  //   return batteryLevel
+  // }
 
 }
