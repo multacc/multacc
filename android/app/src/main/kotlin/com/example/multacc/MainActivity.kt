bb06f30a73380a1b378c0fdb1e384d6093086476
micah-white <mcwhite9@crimson.ua.edu>
2020-04-20 14:35:42 -0500

    Adds capability to send texts; Receives texts but does not store them

--- android/app/src/main/kotlin/com/example/multacc/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/multacc/MainActivity.kt
@@ -14,6 +14,11 @@ import android.os.BatteryManager
 import android.os.Build.VERSION
 import android.os.Build.VERSION_CODES
 import android.provider.Telephony
+import android.telephony.SmsManager
+import android.telephony.SmsMessage
+import android.os.Bundle
+import android.content.BroadcastReceiver
+import android.widget.Toast
 
 import android.util.Log
 import io.flutter.plugin.common.PluginRegistry.Registrar
@@ -31,9 +36,56 @@ class MainActivity: FlutterActivity() {
         setSmsAppIntent.putExtra(Telephony.Sms.Intents.EXTRA_PACKAGE_NAME, packageName)
         startActivityForResult(setSmsAppIntent, 1)
       }
+      // else if (call.method == "encryptSMS"){
+      //   // Encrypts string and encode in Base64
+      //   @Throws(Exception::class)
+      //   fun encrypt(data: String): String {
+      //       val encrypted = cryptoOperation(Cipher.ENCRYPT_MODE, data.toByteArray())
+      //       return Base64.encodeToString(encrypted, Base64.DEFAULT)
+      //   } 
+      // }
+      // else if (call.method == "decryptSMS"){
+      //   // Decrypts string encoded in Base64
+      //   @Throws(Exception::class)
+      //   fun decrypt(encryptedData: String): String {
+      //       val encrypted = Base64.decode( encryptedData, Base64.DEFAULT )
+      //       val decrypted = cryptoOperation(Cipher.DECRYPT_MODE, encrypted)
+      //       return String(decrypted)
+      //   }
+      // }
+      else if (call.method == "sendText"){
+        val arguments = call.arguments<Map<String, Any>>()
+
+        val msg: String = arguments["message"] as String
+        val num: String = arguments["num"] as String
+
+        val smsManager = SmsManager.getDefault() as SmsManager
+        smsManager.sendTextMessage(num, null, msg, null, null)
+
+        result.success(5)
+      }
       else {
         result.notImplemented()
       }
+    }
+  }
+}
+
+class SmsReceiver : BroadcastReceiver() {
+  override fun onReceive(context: Context?, intent: Intent?) {
+    // Get SMS map from Intent
+    if(context == null || intent == null || intent.action == null){
+        return
+    }
+    if (intent.action != (Telephony.Sms.Intents.SMS_RECEIVED_ACTION)) {
+        return
+    }
+    val contentResolver = context.contentResolver
+    val smsMessages = Telephony.Sms.Intents.getMessagesFromIntent(intent)
+    for (message in smsMessages) {
+        Toast.makeText(context, "Message from ${message.displayOriginatingAddress} : body ${message.messageBody}", Toast.LENGTH_SHORT)
+            .show()
+        putSmsToDatabase(contentResolver, message)
     }
   }
 }
