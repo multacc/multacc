5bc10ce3f56e215ce822e144bdb0837722039d0d
micah-white <mcwhite9@crimson.ua.edu>
2020-04-22 19:35:03 -0500

    Add unit tests for TextItem

--- test/items/text_test.dart
+++ test/items/text_test.dart
@@ -1,30 +1,30 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:multacc/items/snapchat.dart';
+import 'package:multacc/items/text.dart';
 import 'package:multacc/items/item.dart';
 
 void main() {
-  SnapchatItem item;
+  TextItem item;
 
-  group('creating empty SnapchatItem', () {
+  group('creating empty TextItem', () {
     setUp(() {
-      item = SnapchatItem();
+      item = TextItem();
     });
     test('initial key should be null', () {
       expect(item.key, null);
     });
-    test('snapchat should be null in json', () {
-      expect(item.toJson()['username'], null);
+    test('text should be null in json', () {
+      expect(item.toJson()['v'], null);
     });
     test('key should initially be null in json', () {
       expect(item.toJson()[ITEM_KEY_KEY], null);
     });
     test('updating username using field should update json', () {
-      item.username = 'MicahWhite';
-      expect(item.toJson()['username'], 'MicahWhite');
+      item.text = 'Micah White';
+      expect(item.toJson()['v'], 'Micah White');
     });
     test('updating username using value setter should update json', () {
-      item.value = 'MicahWhite';
-      expect(item.toJson()['username'], 'MicahWhite');
+      item.value = 'Micah White';
+      expect(item.toJson()['v'], 'Micah White');
     });
     test('updating key should update json', () {
       item.key = '69';
@@ -34,31 +34,31 @@ void main() {
       expect(item.humanReadableValue, '');
     });
     test('human-readable value should be username', () {
-      item.username = 'Mayank Agarwal';
+      item.text = 'Mayank Agarwal';
       expect(item.humanReadableValue, 'Mayank Agarwal');
     });
-    test('type should be Snapchat', () {
-      expect(item.type, MultaccItemType.Snapchat);
+    test('type should be Text', () {
+      expect(item.type, MultaccItemType.Text);
     });
-    test('type should be Snapchat in json', () {
-      expect(item.toJson()[ITEM_TYPE_KEY], 'Snapchat');
+    test('type should be Text in json', () {
+      expect(item.toJson()[ITEM_TYPE_KEY], 'Text');
     });
     test('icon should not be overridden from type default', () {
       expect(item.icon.icon, item.type.icon.icon);
     });
-    test('username should be launchable', () {
-      expect(item.isLaunchable, true);
+    test('text should not be launchable', () {
+      expect(item.isLaunchable, false);
     });
   });
 
-  group('creating SnapchatItem from json', () {
+  group('creating TextItem from json', () {
     setUp(() {
       Map<String, dynamic> map = Map();
-      map['username'] = 'John Cena';
-      item = SnapchatItem.fromJson(map);
+      map['v'] = 'John Cena';
+      item = TextItem.fromJson(map);
     });
-    test('username should be loaded from json', () {
-      expect(item.username, 'John Cena');
+    test('text should be loaded from json', () {
+      expect(item.text, 'John Cena');
     });
   });
 }
